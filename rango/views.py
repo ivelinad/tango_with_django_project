533d7d5889adc1cb2f8965d49305b9b60e58e2b3
Ivelina <2265173d@student.gla.ac.uk>
2018-02-01 19:56:41 +0000

    Chapter 5 finished

--- rango/views.py
+++ rango/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
+from rango.models import Category
+
+# Create your views here.
 from django.http import HttpResponse
-
 def index(request):
-
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-
     return render(request, 'rango/index.html', context=context_dict)
-    #return HttpResponse('Rango says hey there partner! <br/> <a href="/rango/about/">About</a>')
-
+   #return HttpResponse('Rango says hey there partner!<br/><a href="/rango/about/">About</a>')
 def about(request):
-
-	context_dict = {'boldmessage': "Rango says here is the about page."}
-
-	return render(request, 'rango/about.html', context=context_dict)
-    #return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')
+    context_dict = {'boldmessage': "This tutorial has been put together by Ivelina Doynova"}
+    return render(request, 'rango/about.html', context=context_dict)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,11 +21,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^rango/', include('rango.urls')),
-    # above maps any URLs starting
-    # with rango/ to be handled by
-    # the rango application
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.about, name='about'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+   url(r'^$', views.index, name='index'),
+   url(r'^rango/', include('rango.urls')),
+# above maps any URLs starting
+# with rango/ to be handled by
+# the rango application
+   url(r'^admin/', admin.site.urls),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
