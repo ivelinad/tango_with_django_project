590f81bab03d6326a7915fb182d1df218ffeb6dd
Ivelina <2265173@student.gla.ac.uk>
2018-02-02 04:51:02 +0200

    Final version of Chapter 3  and 4

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,8 @@
+
 from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/$', views.about, name='about')
+    url(r'about/$', views.about, name='about')
 ]
--- rango/views.py
+++ rango/views.py
@@ -4,9 +4,10 @@ from rango.models import Category
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
-   #return HttpResponse('Rango says hey there partner!<br/><a href="/rango/about/">About</a>')
+	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+	return render(request, 'rango/index.html', context=context_dict)
+	#return HttpResponse('Rango says hey there partner!<br/><a href="/rango/about/">About</a>')
 def about(request):
-    context_dict = {'boldmessage': "This tutorial has been put together by Ivelina Doynova"}
-    return render(request, 'rango/about.html', context=context_dict)
+	context_dict = {'boldmessage': "This tutorial has been put together by Ivelina Doynova"}
+	return render(request, 'rango/about.html', context=context_dict)
+
