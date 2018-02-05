2a687cd97924020dd6e7794ddaac282e3a1caefd
Ivelina <ivelina.doynova@gmaiil.com>
2018-02-05 23:33:38 +0200

    Up to 8.4

--- rango/urls.py
+++ rango/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'about/$', views.about, name='about'),
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page')
 ]
