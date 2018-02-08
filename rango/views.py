b5cc357fde8f77935e818632ec263d5ee5f4f478
Ivelina <2265173d@student.gla.ac.uk>
2018-02-08 15:31:20 +0000

    Chapter 10 fixed

--- rango/views.py
+++ rango/views.py
@@ -143,6 +143,5 @@ def visitor_cookie_handler(request):
         visits  =  visits  + 1
         request.session['last_visit']  =  str(datetime.now())
     else:
-        visits = 1
         request.session['last_visit']=last_visit_cookie
     request.session['visits'] = visits
